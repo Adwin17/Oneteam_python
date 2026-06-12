9e272ab409ee2fb9d5aa73ecef38f541cd7ed8a5
Adwin17 <adwinjaison30@gmail.com>
2026-06-12 09:45:44 +0530

    Initial Commit

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+s='Adwin' 
+l=['Adwin',18,['Oneteam','Edappaly'],88]
+t=tuple(l)
+
+print(type(s))
+print(type(l))
+print(t)
+
+t[2][1]=20
+print(t)
+
+c=7j
+print(type(c))
+
+# boolean 
+
+is_ok=True
+print(type(is_ok))
+
+
---
+++ operators.py
@@ -0,0 +1,18 @@
+a=10
+b=20
+print(a+b)
+print(a*b)
+
+print(b/a)
+
+a+=5
+print(a)
+
+a*=2
+print(a)
+
+print(a+b)
+
+x=['adwin']
+y=['adwin']
+print(x is y)
