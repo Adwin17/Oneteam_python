a928b7cf455712da691d113772e5c444cc44b7ed
Adwin17 <adwinjaison30@gmail.com>
2026-06-15 10:41:17 +0530

    match statement and for loop

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -10,6 +10,17 @@ a=int(input("Enter the first number:"))
 b=int(input("Enter the Second number:"))
 print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n")
 choice=int(input("Enter the choice:"))
+match choice:
+    case 1:
+        print(a+b)
+    case 2:
+        print(a-b)
+    case 3:
+        print(a*b)
+    case 4:
+        print(a/b)
+
+'''
 if choice==1:
     print(a+b)
 elif choice==2:
@@ -20,4 +31,5 @@ elif choice==4:
     print(a//b)
 else:
     print("invalid input")
+'''
 
---
+++ forloopintro.py
@@ -0,0 +1,40 @@
+'''
+for i in "python":
+    print(i)
+x=['Oneteam','Kochi',['edapally',96]]
+for i in x:
+    print(i)
+for j in {'name':'adwin','age':18}:
+    print(j)
+for k in ("oneteam",96,5):
+    print(k)
+for l in {96,5,6}:
+    print(l)
+'''
+
+'''
+for i in range(10,-2):
+    print(i) 
+'''
+
+'''
+num=int(input("Enter the number to find the factorial:"))
+x=1
+for i in range(1,num+1):
+    x=x*i
+print(f"The factorial of {num} is {x}")
+'''
+'''
+numbers=[45,12,23,4,8,34]
+for i in numbers:
+    if i%2==0:
+        print(i)
+'''
+'''
+x=int(input("Enter how many numbers to be added to the list:"))
+l=[]
+for i in range(x):
+    n=int(input("Enter the number:"))
+    l=l+[n]
+print(l)
+'''
