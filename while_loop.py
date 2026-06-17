3b21e629acb667da0c3431d150348b62bb6f0f9f
Adwin17 <adwinjaison30@gmail.com>
2026-06-17 10:49:40 +0530

    Pattern printing and sorting numbers

--- while_loop.py
+++ while_loop.py
@@ -43,7 +43,56 @@ while x=="yes":
 else:
     print("Exiting...")
 '''
+# print the fruits starting with vowel
+'''
 L=['Apple','Orange','Watermelon','Banana']
 for i in L:
     if i[0] in 'aeiouAEIOU':
         print(i)
+'''
+#list slicing
+'''
+L=[25,45,1,2,3,6]
+print(L[1:3])
+'''
+#character count from string
+'''
+x=input("Enter the string:")
+d={}
+for i in x:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+
+for j in d:
+    # print(j,d[j])
+    print(f"{j} = {d[j]} ")
+'''
+#sorting numbers without builtin
+'''
+numbers=[19,2,1,23,8]
+n=len(numbers) #to find the length of given list -->5
+for k in range(n-1): #Index position dtarts from 0 so range is 5-1-->4
+    for v in range(n-k-1):  
+        if numbers[v]>numbers[v+1]:
+            numbers[v],numbers[v+1]=numbers[v+1],numbers[v]
+print(numbers)   
+'''
+'''
+l=[]
+n=int(input("Enter how many numbers to be entered:"))
+for i in range(n):
+    m=int(input("Enter the number:"))
+    l=l+[m]
+n=len(l)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if l[j]>l[j+1]:
+            l[j],l[j+1]=l[j+1],l[j]
+print(l)
+'''
+
+
+
+
